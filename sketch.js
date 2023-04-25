69989df282dd121f500da0dff89e20d739262c81
LuizGLM <126780076+LuizGLM@users.noreply.github.com>
2023-04-24 22:01:38 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,73 +1,75 @@
-const Engine = Matter.Engine;//usado para criar o motor de física 
-const World = Matter.World;//é usado para criar um mundo físico e para adcionar objetos a ele
-const Bodies = Matter.Bodies;// usado para criar os objetos físicos que habitam o mundo 
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-let engine;
-let world;
-var ball;
 var ground;
-var wedge;
-var angle=60;
-var poly;
-var wall;
 
+function preload()
+{ 
+	
+}
 
 function setup() {
-  createCanvas (400,400);
-  engine = Engine.create();
-  world = engine.world;
+	createCanvas(750, 620);
 
+	engine = Engine.create();
+	world = engine.world;
 
-  //propiedade de uma bola 
-   var ball_options = {
-    restitution: 0.95,  //o quão saltitante ou elastica a bola é
-   
-    frictionAir: 0.01 // o atrito do ar
+	//Crie os Corpos Aqui.
+	var options = {
+		isStatic: true
+	  }
 
-   }
+	  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+	  World.add(world, ground);
 
-   var ops = {// para sprite estaticos
-   isStatic:true
-   };
+	block1 = Bodies.circle(220,10,10,block1_options);
+	World.add(world,block1);
 
-   var ground_options = {
-    isStatic:true 
-   };
+	block2 = Bodies.rectangle(110,50,10,10,block2_options);
+    World.add(world,block2);
+
+	block3 = Bodies.rectangle(350,50,10,10,block3_options);
+	World.add(world,block3);
+
+	var block1_options= {
+		restitution:5,
+		friction:5,
+		frictionAir:5
+	}
+
+	var block2_options = {
+		restitution:0.7,
+		friction:0.01,
+		frictionAir:0.1
+	}
+
+	var block3_options = {
+		restitution:0.01,
+		friction:1,
+		frictionAir:0.3
+	}
+
+	Engine.run(engine);
   
-   ball = Bodies.circle(100,10,20,ball_options);
-   World.add(world,ball);
+}
 
-   wedge = Bodies.rectangle(100,200,100,20,ops);
-   World.add(world,wedge);
 
-ground = Bodies.rectangle(200,390,400,20,ground_options);
-World.add(world,ground);
-
-wall = Bodies.rectangle(300,150,70,10,ground_options);
-World.add(world,wall);
-
+function draw() {
   rectMode(CENTER);
-  ellipseMode(RADIUS);
-}
+  background("gray");
 
-
-function draw() 
-{
- background(51);
- Engine.update(engine);
- ellipse(ball.position.x,ball.position.y,20);
-
- Matter.Body.rotate(wedge,angle)
- push();
-translate(wedge.position.x,wedge.position.y);
-rotate(angle);
-rect(0,0,100,20);
-pop();
-angle +=0.4;
-
-rect(ground.position.x,ground.position.y,400,20);
-rect(wall.position.x,wall.position.y,70,20);
+  Engine.update(engine);
   
+ ellipse(block1.position.x,block1.position.y);
+ ellipse(block2.position.x,block2.position.y);
+ ellipse(block3.position.x,block3.position.y);
+
+ rect(ground.position.x, ground.position.y, width * 2, 1);
+ push();
+ pop();
 }
+
+
 
